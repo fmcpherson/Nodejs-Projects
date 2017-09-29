1811a295bb69fdb2c75cd23ff1be65846ba55e16
Frank McPherson <fmcpherson@users.noreply.github.com>
2017-09-29 17:25:53 -0400

    Test from Working Copy
    Added line of code via Working Copy

--- helloworld.js
+++ helloworld.js
@@ -1,3 +1,4 @@
 // My first app
 console.log("Hello Frank!");
-console.log("Congrats with setting up Workspaces")
+console.log("Congrats with setting up Workspaces");
+console.log("I entered this line using Working Copy");
