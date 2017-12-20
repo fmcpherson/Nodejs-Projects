e7116bcbbaca15828bbeb89b9a2487d530174e36
Frank McPherson <fmcpherson@users.noreply.github.com>
2017-12-20 14:47:02 -0500

    Quick edit on iPad
    Refreshing the memory of my development environment

--- helloworld.js
+++ helloworld.js
@@ -1,4 +1,4 @@
 // My first app
 console.log("Hello Frank!");
 console.log("I edited this line using Atom in my work space");
-console.log("I entered this line using Working Copy");
+console.log("I edited this line using Working Copy on 12/20/2017");
