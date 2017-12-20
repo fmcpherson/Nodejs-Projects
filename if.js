577df6589851042e9af5e8722eee8960729cb69b
Frank McPherson <fmcpherson@users.noreply.github.com>
2017-12-20 16:10:27 -0500

    Initial create

---
+++ if.js
@@ -0,0 +1,10 @@
+// Create new variable called friend = Ray
+var friend = 'Ray';
+
+if (friend === 'Ray' || friend === 'Bob') {
+    console.log('Hello friend');
+} else if (friend === 'Mark' || friend === 'Staci') {
+    console.log('Hello');
+} else {
+    console.log('Whatever');
+}   
