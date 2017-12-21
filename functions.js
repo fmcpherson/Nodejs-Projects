c9d3174f0bd3c8858931eaaae563585f029db8f6
Frank McPherson <fmcpherson@users.noreply.github.com>
2017-12-21 14:10:10 -0500

    Added more calls

--- functions.js
+++ functions.js
@@ -7,4 +7,7 @@ function add (a, b){
 }
 
 var sum = add(6, 4);
-console.log('The sum = ' + sum);
+console.log('The sum of 6 + 4 = ' + sum);
+
+console.log('The sum of 5 + 7 = ' + add(5, 7));
+console.log('The sum of 3 + 4 = ' + add(3, 4));
