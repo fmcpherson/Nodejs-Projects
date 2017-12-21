e638979ba091c4eb218aec47f230cdeb1ac3eb46
Frank McPherson <fmcpherson@users.noreply.github.com>
2017-12-21 14:26:09 -0500

    Deleted a property on an object

--- objects.js
+++ objects.js
@@ -2,6 +2,11 @@ var person = {};
 
 person.firstName = 'Frank';
 person.lastName = 'McPherson';
+person.age = 51;
+
+delete person.age;
+
+console.log(person);
 
 function greetUser (person){
 	console.log('Hello ' + person.firstName + " " + person.lastName);
