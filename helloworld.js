6ca6abe325cb9dd0f8c18c590d9b92619f4905c4
Frank McPherson <frank.mcpherson@gmail.com>
2018-11-23 13:51:24 -0500

    Added a line from my Pixelbook

--- helloworld.js
+++ helloworld.js
@@ -2,3 +2,4 @@
 console.log("Hello Frank!");
 console.log("I edited this line using Atom in my work space");
 console.log("I edited this line using Working Copy on 12/20/2017");
+console.log("I have added this line using Atom on my Pixelbook");
