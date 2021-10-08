b5da77840d35e461865fb7d7441ce99e1bccee9a
Frank McPherson <frank.mcpherson@gmail.com>
2021-10-08 11:46:59 -0400

    Test on Win 11

--- hellofrank.js
+++ hellofrank.js
@@ -1,3 +1,4 @@
 // Quick test of code
 console.log('Hello Frank!');
-console.log('How are you doing today?')
+console.log('How are you doing today?');
+console.log('I hope that you are having a nice day.');
